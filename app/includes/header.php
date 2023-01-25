9df6e0ea6c472e7e6385080e00e5b72b22d634fb
Myron <josephonyekachi77@gmail.com>
2023-01-24 22:04:22 -0800

    updated

---
+++ app/includes/header.php
@@ -0,0 +1,38 @@
+<!-- Spinner Start -->
+<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-primary sticky-top p-0">
+        <a href="index.html" class="navbar-brand bg-primary d-flex align-items-center px-4 px-lg-5">
+            <h2 class="mb-2 text-white">Logistica</h2>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Services</a>
+                    <div class="dropdown-menu fade-up m-0">
+                        <a href="price.html" class="dropdown-item">AIR SERVICE</a>
+                        <a href="service.html" class="dropdown-item">ROAD SERVICE</a>
+                        <a href="quote.html" class="dropdown-item">STORAGE SERVICE</a>
+                        <a href="feature.html" class="dropdown-item">WAREHOUSE SERVICE</a>
+                        
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Tracking</a>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <h4 class="m-0 pe-lg-5 d-none d-lg-block"><a href="#"><i class="fa fa-envelope text-primary me-3"></i></a></h4>
+        </div>
+    </nav>
+    <!-- Navbar End -->
