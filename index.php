11251ae899bf0724e7a692762936f48f71b705d1
Myron <josephonyekachi77@gmail.com>
2023-01-24 22:15:15 -0800

    updated

--- index.php
+++ index.php
@@ -74,52 +74,7 @@ include 'path.php';
 
 
     <!-- Quote Start -->
-    <div class="container py-5 px-4">
-        <div class="container ">
-            <div class="row g-5 align-items-center">
-                <div class="col-lg-5 wow fadeInUp p-0 pic" data-wow-delay="0.1s">
-                    <img class="fluid" src="assets/img/about.jpg" alt="">
-                </div>
-                <div class="col-lg-7 p-0">
-                    <div class="bg-light text-center p-4 wow fadeIn" data-wow-delay="0.5s">
-                        <h3 class="mb-4 text-left">REQUEST A FREE QUOTE</h3>
-                        <form>
-                            <div class="row g-3">
-                                <div class="col-12 col-sm-6">
-                                    <select class="form-select border-0" style="height: 55px;">
-                                        <option selected>SELECT A FREIGHT</option>
-                                        <option value="1">Air</option>
-                                        <option value="2">Sea</option>
-                                        <option value="3">Road</option>
-                                    </select>
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <input type="email" class="form-control border-0" placeholder="EMAIL" style="height: 55px;">
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="CITY DEPARTURE" style="height: 55px;">
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="TOTAL WEIGHT" style="height: 55px;">
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="DELIVERY CITY" style="height: 55px;">
-                                </div>
-                                <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="TIME" style="height: 55px;">
-                                </div>
-                                
-                                
-                                <div class="col-12">
-                                    <button class="btn btn-primary w-100 py-3" type="submit">SUBMIT REQUEST</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include(ROOT_PATH . "/app/includes/quote.php"); ?>
     <!-- Quote End -->
 
 
