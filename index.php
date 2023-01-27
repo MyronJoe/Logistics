6eb6da6cb11eb5f3dc634c42396f389bef3e4c01
Myron <josephonyekachi77@gmail.com>
2023-01-27 00:39:04 -0800

    updated

--- index.php
+++ index.php
@@ -4,18 +4,32 @@ include 'functions.php';
 
 // dd($_SERVER)
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 // dd($uri);
 
-if ($uri === '/') {
-    require 'views/home.php';
+// if ($uri === '/') {
+//     require 'views/home.php';
 
-}elseif($uri === '/about'){
-    require 'views/about.php';
+// }elseif($uri === '/about'){
+//     require 'views/about.php';
 
-}elseif($uri === '/404'){
-    echo 'Not found';
+// }elseif($uri === '/404'){
+//     echo 'Not found';
+// }
+
+$routes = [
+    '/' => 'views/home.php',
+
+    '/about' => 'views/about.php',
+
+    '/tracking' => 'views/tracking.php',
+
+    '/contact' => 'views/contact.php'
+];
+
+if (array_key_exists($uri, $routes)) {
+     require $routes[$uri];
 }
 
 
