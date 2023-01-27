e5aa715a4ce34e16023712a49b28437ccc4bd8db
Myron <josephonyekachi77@gmail.com>
2023-01-27 01:04:38 -0800

    updated

--- index.php
+++ index.php
@@ -29,14 +29,19 @@ $routes = [
 ];
 
 
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-}else{
-    http_response_code(404);
+function abort($code = 404){
+    http_response_code($code);
 
     require 'views/error.php';
 
     die();
+}
+
+
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+}else{
+    abort();
 }
 
 
