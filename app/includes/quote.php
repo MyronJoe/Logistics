82457c6de1a6e3d0a1f51b5562f7634e314846a3
Myron <josephonyekachi77@gmail.com>
2023-01-27 02:00:13 -0800

    updated

--- app/includes/quote.php
+++ app/includes/quote.php
@@ -10,7 +10,7 @@
                         <form>
                             <div class="row g-3">
                                 <div class="col-12 col-sm-6">
-                                    <select class="form-select border-0" style="height: 55px;">
+                                    <select class="noRadius form-select border-0" style="height: 55px;">
                                         <option selected>SELECT A FREIGHT</option>
                                         <option value="1">Air</option>
                                         <option value="2">Sea</option>
@@ -18,24 +18,24 @@
                                     </select>
                                 </div>
                                 <div class="col-12 col-sm-6">
-                                    <input type="email" class="form-control border-0" placeholder="EMAIL" style="height: 55px;">
+                                    <input type="email" class="noRadius form-control border-0" placeholder="EMAIL" style="height: 55px;">
                                 </div>
                                 <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="CITY DEPARTURE" style="height: 55px;">
+                                    <input type="text" class="noRadius form-control border-0" placeholder="CITY DEPARTURE" style="height: 55px;">
                                 </div>
                                 <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="TOTAL WEIGHT" style="height: 55px;">
+                                    <input type="text" class="noRadius form-control border-0" placeholder="TOTAL WEIGHT" style="height: 55px;">
                                 </div>
                                 <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="DELIVERY CITY" style="height: 55px;">
+                                    <input type="text" class="noRadius form-control border-0" placeholder="DELIVERY CITY" style="height: 55px;">
                                 </div>
                                 <div class="col-12 col-sm-6">
-                                    <input type="text" class="form-control border-0" placeholder="TIME" style="height: 55px;">
+                                    <input type="text" class="noRadius form-control border-0" placeholder="TIME" style="height: 55px;">
                                 </div>
                                 
                                 
                                 <div class="col-12">
-                                    <button class="btn btn-primary w-100 py-3" type="submit">SUBMIT REQUEST</button>
+                                    <button class="noRadius btn btn-primary w-100 py-3" type="submit">SUBMIT REQUEST</button>
                                 </div>
                             </div>
                         </form>
