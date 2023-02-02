45956bdab47f50807d3932a96fca11518583bdc4
Myron <josephonyekachi77@gmail.com>
2023-02-01 21:50:39 -0800

    updated

--- app/database.php/db.php
+++ app/database.php/db.php
@@ -11,5 +11,16 @@
         exit;
     }
 
+    //execute the querry for selectone and selectall fn
+    function executeQuery($sql, $data){
+        global $conn;
+        $stmt = $conn->prepare($sql);
+        $values = array_values($data);
+        $types = str_repeat('s', count($values));
+        $stmt->bind_param($types, ...$values);
+        $stmt->execute();
+        return $stmt;
+    }
+ 
     
 ?>
