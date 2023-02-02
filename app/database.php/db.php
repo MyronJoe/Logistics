71ed4493a2ed324822d024dc0f1cfa2de4c77b39
Myron <josephonyekachi77@gmail.com>
2023-02-01 21:50:10 -0800

    updated

---
+++ app/database.php/db.php
@@ -0,0 +1,15 @@
+<?php
+    require_once(ROOT_PATH . "/app/includes/session.php");
+    
+    require 'connect.php';
+
+    //funtion for texting - will be deleted
+    function dump($value){
+        echo '<pre>';
+            print_r($value);
+        echo '</pre>';
+        exit;
+    }
+
+    
+?>
