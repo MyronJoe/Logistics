c1e90a540d9f989af4300d6c0ec2455814a8e06b
Myron <josephonyekachi77@gmail.com>
2023-02-01 21:51:51 -0800

    updated

--- app/database.php/db.php
+++ app/database.php/db.php
@@ -55,5 +55,30 @@
         
     }
 
-    
+    //select one based on a condition
+    function selectOne($table, $conditions){
+        global $conn;
+        $sql = "SELECT * FROM $table";
+        $i = 0;
+        foreach ($conditions as $key => $value) {
+
+            if ($i === 0) {
+                $sql = $sql . " WHERE $key = ?";
+            }else{
+                $sql = $sql . " AND $key = ?";
+            }
+            $i++;
+        }
+
+        //RETURNS THE FIRST CONDITION FOUND
+        $sql = $sql . " LIMIT 1";
+
+        $stmt = executeQuery($sql, $conditions);
+        $records = $stmt->get_result()->fetch_assoc();
+        return $records;
+        
+    }
+
+
+   
 ?>
