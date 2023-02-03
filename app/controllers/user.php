990abc14d43cd48bced850a3b11219ca81927373
Myron <josephonyekachi77@gmail.com>
2023-02-02 23:16:50 -0800

    updated

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -14,7 +14,23 @@
 
     $admin_users = selectAll($table);
 
-    
+    //function that logs the user in
+    function loginUser($user){
+        $_SESSION['id'] = $user["id"];
+        $_SESSION['username'] = $user["username"];
+        $_SESSION['email'] = $user["email"];
+        $_SESSION['admin'] = $user["admin"];
+        $_SESSION['message'] = "You are now logged in";
+        $_SESSION['type'] = "success";
 
+        if ($_SESSION['admin']) {
+            header("location: " . BASE_URL . "/admin/dashboard.php");
+        }else{
+            header("location: " . BASE_URL . "/index.php");
+        }
+        exit();
+    }
+
+    
 
 ?>
